ab010f3e6217fddd305768f7fd115fcdec3ede1d
Samuel <samuelwestlake@hotmail.co.uk>
2017-05-04 20:53:42 +0100

    fork rosrun serial_node

--- buggy.sh
+++ buggy.sh
@@ -12,7 +12,7 @@ export ROS_IP=$local_ip
 export ROS_HOSTNAME=$local_ip
 
 cd catkin_ws/src/multi_tier_robot_system/src/
-rosrun multi_tier_robot_system serial_node.py /dev/ttyACM0
+rosrun multi_tier_robot_system serial_node.py /dev/ttyACM0 &
 rosrun multi_tier_robot_system encoder_node.py
 
 
